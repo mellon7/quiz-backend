b19c97ff15b7d0fdea28a1bfb1dce1492a1b9ca8
Fuwa <toryaki.corp@gmail.com>
2023-08-24 16:21:44 +0200

    Small fix

--- server.cjs
+++ server.cjs
@@ -112,7 +112,8 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
     }
   })
 );
